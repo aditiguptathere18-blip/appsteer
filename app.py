aece3eca64e8c1da1f4f4f12335efb1124ad9db9
Aditi Gupta <aditiguptathere18@gmail.com>
2026-04-14 17:57:48 +0530

    Login functionality added

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for,session
 
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
 
 @app.route('/')
-def login():
+def login_page():
     return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if email == "admin@gmail.com" and password == "1234":
+        session['user'] = email
+        return redirect(url_for('dashboard'))
+    else:
+        return render_template('login.html',error="Invalid email or password",email=email)
+    
+@app.route('/dashboard')
+def dashboard():
+        if 'user' in session:
+          return render_template('dashboard.html')
+        else:
+            return redirect(url_for('login_page'))
 
 if __name__ == '__main__':
     app.run(debug=True)
